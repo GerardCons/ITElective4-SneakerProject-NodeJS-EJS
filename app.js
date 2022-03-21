62f3f47f7522caec3092ee39da152d23bd2a2060
GerardCons <consolaciongerardian@gmail.com>
2022-03-21 11:40:43 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+const fs = require('firebase-admin');
+
+const serviceAccount = require('./itelective4-firebase-key.json');
+
+const port = 8080
+
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/images', express.static(__dirname + 'public/images'))
+
+fs.initializeApp({
+    credential: fs.credential.cert(serviceAccount)
+});
+
+const db = fs.firestore();
+const itemColl = db.collection('items');
+
+app.get('/', async function (req, res) {
+    const items = await itemColl.get();
+    let data = {
+        url: req.url,
+        itemData: items.docs,
+        array: [
+            { fname: "Dodong", lname: "Laboriki" }
+        ],
+    }
+    res.render('pages/index', data);
+});
+
+app.listen(port);
